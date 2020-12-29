cc8b8826753ed0ed7c5fd68b117beb113c367951
João Henrique Buso Bortolotto <jhbbortolotto@MacBook-Pro-de-Joao.local>
2020-12-28 21:59:40 -0300

    Observables.

--- rxjava/app/src/main/java/com/jhbb/rxjava/MainActivity.kt
+++ rxjava/app/src/main/java/com/jhbb/rxjava/MainActivity.kt
@@ -2,10 +2,61 @@ package com.jhbb.rxjava
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jhbb.rxjava.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.core.Observable
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnObservable.setOnClickListener { observableClick() }
+        binding.btnObservableCreate.setOnClickListener { observableCreateClick() }
+    }
+
+    private fun observableClick() {
+        Observable.just(
+            "Brasil \n",
+            "Alemanha \n",
+            "Rússia \n",
+            "China \n",
+            "Austrália \n",
+            "Canadá \n")
+                .subscribe {
+                    binding.txtPanel.append(it.toString())
+                }
+    }
+
+    private fun observableCreateClick() {
+        val countries = listOf(
+                "Brasil \n",
+                "Alemanha \n",
+                "Rússia \n",
+                "China \n",
+                "Austrália \n",
+                "Canadá \n")
+
+        val observableCountries = Observable.create<String> {
+            emitter ->
+                countries.forEachIndexed {
+                        index, c ->
+                            if (index.equals(3)) {
+                                emitter.onError(Exception("Erro"))
+                            }
+                            emitter.onNext(c)
+                }
+            emitter.onComplete()
+        }
+
+        observableCountries
+            .subscribe(
+                { binding.txtPanel.append(it.toString()) },
+                { binding.txtPanel.append("ERROR") },
+                { binding.txtPanel.append("COMPLETED") }
+        )
     }
 }
