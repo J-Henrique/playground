2d9584a8962a39239f9bac921c055fe1a414b19f
João Henrique <joao.bortoloto@dextra-sw.com>
2020-12-30 15:16:09 -0300

    Mocking server

---
+++ rxjava/server/server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const countries = {
+	1: {
+        id: 1,
+		name: 'Brazil',
+		language: 'Portuguese',
+		continent: 'South America',
+		capital: 'Brasilia'
+	},
+
+	2: {
+        id: 2,
+		name: 'Canada',
+		language: 'English, French',
+		continent: 'North America',
+		capital: 'Ottawa'
+	},
+
+	3: {
+        id: 3,
+		name: 'Germany',
+		language: 'German',
+		continent: 'Europe',
+		capital: 'Berlin'
+	},
+
+	4: {
+        id: 4,
+		name: 'Japan',
+		language: 'Japanese',
+		continent: 'East Asia',
+		capital: 'Tokyo'
+	},
+
+	5: {
+        id: 5,
+		name: 'Israel',
+		language: 'Hebrew',
+		continent: 'Middle East',
+		capital: 'Jerusalem'
+	}
+}
+
+const parseToArray = (map) => {
+	let list = []
+	Object.keys(map).forEach(key => list.push(map[key]))
+	return list
+}
+
+app.get("/country", function(req, res) {
+    res.json(parseToArray(countries));
+});
+
+app.listen(3000, function() {
+  console.log("Server is running");
+});
