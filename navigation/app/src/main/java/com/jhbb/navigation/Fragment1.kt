ae03cbd478e7eea408ec3956bbdbe95ea55b3f3f
João Henrique <joao.bortolotto@dextra-sw.com>
2021-04-30 08:42:51 -0300

    Navigation sample.

---
+++ navigation/app/src/main/java/com/jhbb/navigation/Fragment1.kt
@@ -0,0 +1,29 @@
+package com.jhbb.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+
+class Fragment1 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btFragment1).setOnClickListener {
+            findNavController().navigate(Fragment1Directions.actionFragment1ToFragment2("Navigation from Fragment 1"))
+        }
+    }
+}
---
+++ navigation/app/src/main/java/com/jhbb/navigation/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.jhbb.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val host = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navigationController = host.navController
+        val appBarConfiguration = AppBarConfiguration(navigationController.graph)
+
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navigationController, appBarConfiguration)
+    }
+}
